1512f4baccd87c082537195125294e04d05ee9b1
James Kirkby <jkirkby@protonmail.ch>
2017-12-07 06:07:45 +0100

    namespace bracing

--- src/SchemaContract.php
+++ src/SchemaContract.php
@@ -1,24 +1,26 @@
 <?php
+	declare(strict_types=1);
 
-namespace Jkirkby91\Boilers\SchemaBoilers;
+	namespace Jkirkby91\Boilers\SchemaBoilers {
 
-/**
- * Class SemanticContract
- *
- * @package app\Contracts
- * @author James Kirkby <me@jameskirkby.com>
- */
-interface SchemaContract
-{
+		/**
+		 * Interface SchemaContract
+		 *
+		 * @package Jkirkby91\Boilers\SchemaBoilers
+		 * @author  James Kirkby <jkirkby@protonmail.ch>
+		 */
+		interface SchemaContract
+		{
 
-    /**
-     * @return mixed
-     */
-    public function getName();
+			/**
+			 * @return mixed
+			 */
+			public function getName();
 
-    /**
-     * @param $name
-     * @return mixed
-     */
-    public function setName($name);
-}
+			/**
+			 * @param $name
+			 * @return mixed
+			 */
+			public function setName(string $name);
+		}
+	}
